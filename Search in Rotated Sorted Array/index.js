09b137261d069307f0fb2e932d6b14fd4b8fc423
thuocsi.dungtt <dung.truong@thuocsi.vn>
2023-08-28 22:21:50 +0700

    update search in rotated sorted array to O(logn)

--- Search in Rotated Sorted Array/index.js
+++ Search in Rotated Sorted Array/index.js
@@ -1,35 +1,40 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
 var search = function (nums, target) {
-  const pivot = Math.floor(nums.length / 2);
-  let start = 0;
-  let end = nums.length - 1;
-  if (target === nums[pivot]) {
-    return pivot;
-  }
-  if (target === nums[nums.length - 1]) {
-    return nums.length - 1;
-  }
-  if (target === nums[0]) {
-    return 0;
-  }
+  return rotatedBinarySearch(nums, 0, nums.length - 1, target)
+}
 
-  if (target > nums[pivot] && target < nums[nums.length - 1]) {
-    start = pivot + 1;
-  }
-  if (target > nums[0] && target < nums[pivot]) {
-    start = 0;
-    end = pivot - 1;
-  }
-  for (let i = start; i <= end; i++) {
-    if (nums[i] == target) {
-      return i;
+
+var rotatedBinarySearch = function (nums, start, end, target) {
+  while (true) {
+    const pivot = Math.floor((start + end) / 2);
+    if (target === nums[pivot]) {
+      return pivot;
+    }
+    if (target === nums[end]) {
+      return end;
+    }
+    if (target === nums[start]) {
+      return start;
+    }
+
+    if (
+      (target > nums[start] && target < nums[pivot] && nums[start] < nums[pivot]) ||
+      (target < nums[start] && target < nums[pivot] && nums[start] > nums[pivot]) ||
+      (target > nums[start] && target > nums[pivot] && nums[start] > nums[pivot])) {
+      end = pivot - 1;
+    } else {
+      start = pivot + 1;
+    }
+    if (start >= end) {
+      return -1;
     }
   }
-  return -1;
+  // for (let i = start; i <= end; i++) {
+  //   if (nums[i] == target) {
+  //     return i;
+  //   }
+  // }
+  // return -1;
+  // return rotatedBinarySearch(nums, start, end, target);
 };
 
-console.log(search([7, 8, 1, 2, 3, 4, 5, 6], 2))
+console.log(search([8, 9, 2, 3, 4], 9))
