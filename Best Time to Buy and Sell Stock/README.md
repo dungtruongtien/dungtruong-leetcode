939cfb063f6bc40e9cdc44362b11e4bcd627cc99
thuocsi.dungtt <dung.truong@thuocsi.vn>
2023-09-05 19:59:42 +0700

    update Palindrome Number algorithm to o(n/2)

--- Best Time to Buy and Sell Stock/README.md
+++ Best Time to Buy and Sell Stock/README.md
@@ -1,1 +1,15 @@
 Link problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+1232
+1221
+
+1  4
+ 23
+
+12321
+1   5
+ 2 4
+  3
+
+   Math.log(12321) / Math.log(10);
--- Palindrome Number/index.js
+++ Palindrome Number/index.js
@@ -3,13 +3,22 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  let xCloned = x;
-  let tempNumb = 0;
-  while (xCloned > 0) {
-    tempNumb = (tempNumb * 10) + (xCloned % 10);
-    xCloned = Math.floor(xCloned / 10);
+  if(x >= 0 && x < 10) {
+    return true;
   }
-  return tempNumb === x;
+  if(x < 0 || x % 10 === 0) {
+    return false;
+  }
+  let temp = 0;
+  let flag = Math.floor(Math.log(Math.abs(x)) / Math.log(10)) + 1;
+  while (x > temp) {
+    temp = (temp * 10) + (x % 10);
+    x = Math.trunc(x / 10);
+  }
+  if(flag % 2 !== 0) {
+    return x === Math.floor(temp / 10);
+  }
+  return temp === x;
 };
 
-console.log(isPalindrome(123));
+console.log(isPalindrome(11));
